60bb9c79e0a3df81af45d41af1e7fcb55f0f6b4e
Himanshuranjan19 <himranjan2@gmail.com>
2017-08-18 13:57:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req, res){
+  res.send('Article one is now ready and will be served here. Please Wait!')
+});
+
+app.get('/article-two', function(req, res){
+  res.send('Article two is now ready and will be served here. Please Wait!')
+});
+
+app.get('/article-three', function(req, res){
+  res.send('Article three is now ready and will be served here. Please Wait!')
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
