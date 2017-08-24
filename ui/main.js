e96ab62a2a35f242300bb148af0c67530cbc99bb
Himanshuranjan19 <himranjan2@gmail.com>
2017-08-24 22:53:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 
 //Change the text of the main div
-/*var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';*/
+/**
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';
 
 //Move the image
 var img=document.getElementById('img');
@@ -15,3 +16,10 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';
 };
+*/
+
+/*
+If upper element tobe selected is not found, lower elements are also not
+implemented as supposed to be by JavaScript. In other words, lower
+elements are neglected.
+*/
