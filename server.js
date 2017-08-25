8efe2231ba0007efd29cf3aba24a3001fce321ca
Himanshuranjan19 <himranjan2@gmail.com>
2017-08-25 16:16:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,6 +115,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names = [];
 app.get('/submit-name/:name', function (req, res) {
   //Get the name from request object
   var name = req.param.name;
