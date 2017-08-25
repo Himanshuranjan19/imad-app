f40c452f5118565d2ae13c6b75a7f7f6fa4314e4
Himanshuranjan19 <himranjan2@gmail.com>
2017-08-25 14:34:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,11 +11,11 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('img');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 5;
+    marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function() {
-    var interval = setInterval(moveRight, 20);
+    var interval = setInterval(moveRight, 50);
     //img.style.marginLeft = '100px';
 };
 
